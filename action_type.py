4379da0e621bdf6ab1faa87265da64317e7320ff
Alexey Zaytsev <alex.rv9rh@gmail.com>
2025-10-05 13:36:07 +0500

    Add enums: ActionType, StepResponseType

---
+++ action_type.py
@@ -0,0 +1,8 @@
+
+from enum import Enum
+
+
+class ActionType(Enum):
+    MAKE_STEP = 0
+    STEP_REQUEST = 1
+    STEP_RESPONSE = 2
---
+++ step_response_type.py
@@ -0,0 +1,8 @@
+
+from enum import Enum
+
+
+class StepResponseType(Enum):
+    AWAY = 0
+    WOUNDED = 1
+    KILLED = 2
